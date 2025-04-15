6a46f88cfbac7bae5a7ccdac6b671eb7f96c5b64
gardoffwhite <gardoffwhite@gmail.com>
2025-04-15 20:38:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -44,4 +45,6 @@ def update():
     return render_template('index.html', character_data=character_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # ใช้พอร์ตจากตัวแปรสภาพแวดล้อม PORT หรือใช้พอร์ต 5000 ถ้าไม่มี
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
